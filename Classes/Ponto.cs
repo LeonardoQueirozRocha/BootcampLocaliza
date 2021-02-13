dfc82e1ce51d386ad252fa86504c60ca91e30e70
Leonardo Queiroz Rocha <leonardo.queiroz.rocha@outlook.com>
2021-02-13 15:17:50 -0300

    Exemplo sobre classes

---
+++ Classes/Ponto.cs
@@ -0,0 +1,31 @@
+namespace Classes
+{
+    public class Ponto
+    {
+        public int x, y;
+        private int distancia;
+
+        public Ponto(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        protected void CalculaDistancia()
+        {
+            // Faz alguma coisa
+        }
+
+        private void CalcularDistancia2()
+        {
+            // Faz alguma coisa
+        }
+
+        public virtual void CalcularDistancia3()
+        {
+            // Faz alguma coisa
+        }
+
+
+    }
+}
